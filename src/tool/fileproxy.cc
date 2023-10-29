69338bc825c542df5e4893369850d78e1f01ea7f
Origin-yy <1657615110@qq.com>
2023-10-29 21:24:06 +0800

    文件传输初步

---
+++ src/tool/fileproxy.cc
@@ -0,0 +1,33 @@
+#include "fileproxy.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <memory>
+
+namespace ws {
+
+void FileProxy::Statget() {
+    if (!stat_) {
+        stat_ = std::make_unique<struct stat>();
+        fstat(File_Description, stat_.get());
+    }
+}
+
+__off_t FileProxy::FileSize() {
+    Statget();
+    return stat_->st_size;
+}
+
+bool FileProxy::IsTextFile() {
+    Statget();
+    return (stat_->st_mode & S_IFDIR || stat_->st_mode & S_IFCHR ||
+            stat_->st_mode & S_IFBLK);
+}
+
+FileProxy::~FileProxy() { ::close(File_Description); }
+
+FileProxy::FileProxy(const FileProxy& path1, const char* path2)
+    : File_Description(openat(path1.fd(), path2, O_RDONLY)) {}
+
+}  // namespace ws
---
+++ src/tool/fileproxy.h
@@ -0,0 +1,35 @@
+#ifndef FILEPROXY_H_
+#define FILEPROXY_H_
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <memory>
+
+#include "../base/havefd.h"
+#include "../base/nocopy.h"
+
+namespace ws {
+
+class FileProxy : public Nocopy, public Havefd {
+   public:
+    explicit FileProxy(const char* path)
+        : File_Description(::open(path, O_RDONLY)) {}
+    FileProxy(const FileProxy&, const char*);
+    ~FileProxy() override;
+
+    int fd() const noexcept final { return File_Description; }
+    bool Fd_Good() const noexcept { return File_Description > -1; }
+    __off_t FileSize();
+    bool IsTextFile();
+
+   private:
+    int File_Description;
+    std::unique_ptr<struct stat> stat_ = nullptr;
+    void Statget();
+};
+
+}  // namespace ws
+
+#endif
