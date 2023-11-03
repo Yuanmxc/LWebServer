0af97762072b14a16507fea411d36bc941d8a30e
Origin-yy <1657615110@qq.com>
2023-11-04 00:01:14 +0800

    文件读取

---
+++ src/tool/filereader.cc
@@ -0,0 +1,10 @@
+#include "filereader.h"
+
+#include <sys/sendfile.h>
+namespace ws {
+
+ssize_t FileReader::SendFile(int Socket_Fd) {
+    return sendfile(Socket_Fd, fd(), &offest, length - offest);
+}
+
+}  // namespace ws
